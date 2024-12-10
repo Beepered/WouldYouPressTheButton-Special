a58d693acaf8ac5420c1357446ad22ea5bdcc08e
Beepered <bktrieu@gmail.com>
2024-12-09 17:52:57 -0800

    bigger screen and backgrounds begin

---
+++ wouldyoupressthebutton-special/scripts/background.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+@export var backgroundObj : PackedScene
+
+@onready var timer = $Timer
+
+func _ready() -> void:
+	timer.wait_time = randf_range(15, 30)
+	timer.start()
+
+func _on_timer_timeout() -> void:
+	var obj = backgroundObj.instantiate()
+	add_child(obj)
+	timer.wait_time = randf_range(15, 30)
+	timer.start()
---
+++ wouldyoupressthebutton-special/scripts/background_obj.gd
@@ -0,0 +1,20 @@
+extends ColorRect
+
+@onready var direction = randi_range(0, 1) # 0 left, 1 right
+@onready var xMove = randf_range(15, 25)
+
+func _ready() -> void:
+	var _size = randi_range(200, 400)
+	size = Vector2(_size, _size)
+	var xPos = _size * -1 if direction == 0 else get_viewport().size.x + _size
+	var yPos = randi_range(200, get_viewport().size.y - 200)
+	position = Vector2(xPos, yPos)
+	color = Color(randf(), randf(), randf())
+	modulate.a = randf_range(0.2, 0.5)
+	if direction == 1:
+		xMove *= -1
+
+func _process(delta: float) -> void:
+	rotation += 0.07 * delta
+	position.x += xMove * delta
+	print(position.x)
